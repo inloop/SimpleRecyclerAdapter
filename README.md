82bd5a3dff81a6a89797cbaf1d3897a29bacb40f
Daniel Novak <novak@inloop.eu>
2016-10-07 20:36:09 +0200

    Basic project skeleton

---
+++ README.md
@@ -0,0 +1,13 @@
+SimpleRecyclerAdapter
+================
+
+How to implement
+--------
+
+Download
+--------
+
+Grab via Gradle:
+```groovy
+compile 'eu.inloop:simplerecycleradapter:0.1.0'
+```
---
+++ sample/src/main/java/eu/inloop/simplerecycleradapter/sample/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package eu.inloop.simplerecycleradapter.sample.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import eu.inloop.simplerecycleradapter.sample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
